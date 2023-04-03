20f72597220e7c20f995a6c7e0a9964edd2cbdcc
Ariana Jackson <ariana.desiree@gmail.com>
2023-04-03 10:06:51 -0500

    rest framework

--- backend/tickit_project/tickit/serializers.py
+++ backend/tickit_project/tickit/serializers.py
@@ -1,17 +1,6 @@
 from rest_framework import serializers
 from .models import Venue, Event
 
-class VenueSerializer(serializers.HyperlinkedModelSerializer):
-    events = serializers.HyperlinkedRelatedField(
-        view_name='event_detail',
-        many=True,
-        read_only=True
-    )
-
-    class Meta:
-        model= Venue
-        fields = ('name', 'location', 'occupancy', 'photo_url', 'events')
-
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
     venues = serializers.HyperlinkedRelatedField(
@@ -19,6 +8,27 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
         read_only=True
     )
 
+    venue_id = serializers.PrimaryKeyRelatedField(
+        queryset=Venue.objects.all(),
+        source='venue'
+    )
+
     class Meta:
         model= Event
-        fields = ('venues', 'name', 'artist', 'date', 'price', 'image_url', 'tickets_available')
+        fields = ('id', 'venues', 'venue_id', 'name', 'artist', 'date', 'price', 'image_url', 'tickets_available')
+
+
+class VenueSerializer(serializers.HyperlinkedModelSerializer):
+    events = serializers.HyperlinkedRelatedField(
+        view_name='event_detail',
+        many=True,
+        read_only=True
+    )
+
+    venue_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='venue_detail'
+    )
+
+    class Meta:
+        model= Venue
+        fields = ('name', 'location', 'occupancy', 'photo_url', 'events', 'venue_url')
--- backend/tickit_project/tickit_django/settings.py
+++ backend/tickit_project/tickit_django/settings.py
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'tickit',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -56,6 +58,20 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:5555",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000"
+]
+
 
 
 ROOT_URLCONF = 'tickit_django.urls'
