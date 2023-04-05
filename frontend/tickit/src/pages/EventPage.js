c65694c17821ebd4c4d50438c6ab6da1b5e72e0c
ralp22 <ryan_halpert@yahoo.com>
2023-04-05 14:05:26 -0400

    sticky nav and cleaned up code

--- frontend/tickit/src/pages/EventPage.js
+++ frontend/tickit/src/pages/EventPage.js
@@ -1,34 +1,41 @@
-import logo from '../assets/logo.png'
-import Nav from '../components/Nav'
-import axios from 'axios'
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react'
+import logo from "../assets/logo.png";
+import Nav from "../components/Nav";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function EventPage(props) {
+  const [events, setEvents] = useState(null);
 
-    const [events, setEvents] = useState(null)
+  let { id } = useParams();
 
-    let { id } = useParams()
+  useEffect(() => {
+    let selectedEvent = props.events[id];
+    setEvents(selectedEvent);
+  }, []);
 
-    useEffect(()=>{
-        let selectedEvent = props.events[id];
-        setEvents(selectedEvent)
-    },[])
-
-
-
-    return events && props.events && props.venues ? (
-        <div className="event-page">
-            <img className="tickit-logo" src={logo}/>
-            <Nav/>
-            <img className="venue-backdrop" src={events.image_url}/>
-            <section className="details-container">
-            <h1><span style={{color: 'white'}}>{events.name}</span></h1>
-            <h1><span style={{color: 'white'}}>{events.artist}</span></h1>
-            <h1>{events.date}</h1>
-            <h1>Price: <span style={{color: '#FFBD59'}}>{events.price}</span></h1>
-            </section>
-
-            </div>
-    ) :  <h2 className="loading">Loading . . . </h2>
+  return events && props.events && props.venues ? (
+    <div className="event-page">
+      <img className="tickit-logo" src={logo} />
+      <Nav />
+      <section className="event-details-container">
+        <img className="event-backdrop" src={events.image_url} />
+        <h1>
+          <span style={{ color: "white" }}>{events.name}</span>
+        </h1>
+        <h1>
+          <span style={{ color: "white" }}>{events.artist}</span>
+        </h1>
+        <h1>{events.date}</h1>
+        <h1>
+          Price: <span style={{ color: "#FFBD59" }}>{events.price}</span>
+        </h1>
+        <div style={{ padding: "10px" }}>
+          Available Tickets: {events.tickets_available}{" "}
+          <button className="ticket-button">Buy Now</button>
+        </div>
+      </section>
+    </div>
+  ) : (
+    <h2 className="loading">Loading . . . </h2>
+  );
 }
--- frontend/tickit/src/pages/Home.js
+++ frontend/tickit/src/pages/Home.js
@@ -1,48 +1,56 @@
-import Nav from "../components/Nav"
-import logo from "../assets/venue2.png"
-import Carousel, { CarouselItem } from "../Carousel"
-import { useNavigate } from "react-router-dom"
+import Nav from "../components/Nav";
+import logo from "../assets/venue2.png";
+import Carousel, { CarouselItem } from "../Carousel";
+import { useNavigate } from "react-router-dom";
 
-export default function Venues(props){
+export default function Venues(props) {
+  let navigate = useNavigate();
 
-    let navigate = useNavigate()
+  const showVenue = (id) => {
+    navigate(`/venues/${id}`);
+  };
+  const showEvent = (id) => {
+    navigate(`/events/${id}`);
+  };
 
-    const showVenue = (id) => {
-    navigate(`/venues/${id}`)
-    }
-    const showEvent = (id) => {
-    navigate(`/events/${id}`)
-    }
-
-    return props.venues && props.events ? (
-        <div className="home-page">
-            <img className="tickit-logo2" src={logo}/>
-            <Nav/>
-            <Carousel>
-                {
-                    props.events.map((event, id)=>(
-                        <CarouselItem>
-                            <div onClick={()=>{showEvent(id)}}>
-                            <img className="carousel-img" src={event.image_url}/>
-                            <h1>{event.name}</h1>
-                            </div>
-                        </CarouselItem>
-                    ))
-                }
-            </Carousel>
-            <div className="grid">
-                {props.venues.map((venue, id) => (
-                    <div onClick={()=>{showVenue(id)}} className="card">
-                    <img className="card-img" src={venue.photo_url}/>
-                    <div className="banner">
-                    <div className="venue-info">
-                    <p>{venue.name}</p>
-                    <p>{venue.location}</p>
-                    </div>
-                    </div>
-                    </div>
-                ))}
+  return props.venues && props.events ? (
+    <div className="home-page">
+      <Nav />
+      <img className="tickit-logo2" src={logo} />
+      <Carousel>
+        {props.events.map((event, id) => (
+          <CarouselItem>
+            <div
+              onClick={() => {
+                showEvent(id);
+              }}
+            >
+              <img className="carousel-img" src={event.image_url} />
+              <h1>{event.name}</h1>
             </div>
-        </div>
-    ) :  <h2 className="loading">Loading . . . </h2>
+          </CarouselItem>
+        ))}
+      </Carousel>
+      <div className="grid">
+        {props.venues.map((venue, id) => (
+          <div
+            onClick={() => {
+              showVenue(id);
+            }}
+            className="card"
+          >
+            <img className="card-img" src={venue.photo_url} />
+            <div className="banner">
+              <div className="venue-info">
+                <p>{venue.name}</p>
+                <p>{venue.location}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  ) : (
+    <h2 className="loading">Loading . . . </h2>
+  );
 }
