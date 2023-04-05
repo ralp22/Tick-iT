a33b70cf5d29b65f6374a6eab40784131f32bf6a
Ariana Jackson <ariana.desiree@gmail.com>
2023-04-05 09:18:36 -0500

    event page data rendered

--- frontend/tickit/src/pages/EventPage.js
+++ frontend/tickit/src/pages/EventPage.js
@@ -1,11 +1,34 @@
 import logo from '../assets/logo.png'
 import Nav from '../components/Nav'
+import axios from 'axios'
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react'
 
 export default function EventPage(props) {
-    return props.events && props.venues ? (
+
+    const [events, setEvents] = useState(null)
+
+    let { id } = useParams()
+
+    useEffect(()=>{
+        let selectedEvent = props.events[id];
+        setEvents(selectedEvent)
+    },[])
+
+
+
+    return events && props.events && props.venues ? (
         <div className="event-page">
             <img className="tickit-logo" src={logo}/>
             <Nav/>
+            <img className="venue-backdrop" src={events.image_url}/>
+            <section className="details-container">
+            <h1><span style={{color: 'white'}}>{events.name}</span></h1>
+            <h1><span style={{color: 'white'}}>{events.artist}</span></h1>
+            <h1>{events.date}</h1>
+            <h1>Price: <span style={{color: '#FFBD59'}}>{events.price}</span></h1>
+            </section>
+
             </div>
     ) :  <h2 className="loading">Loading . . . </h2>
 }
