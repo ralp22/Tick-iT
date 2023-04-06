52903e6bb7035c1dce7067b34f1fb493d2420716
Faatimah Cleveland <faatimahcleveland@gmail.com>
2023-04-05 22:29:47 -0400

    Auth on backend complete

--- backend/tickit_project/tickit_django/urls.py
+++ backend/tickit_project/tickit_django/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tickit.urls')),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/auth/',include("account.urls", namespace='account')),
 ]
